6201135931f7b18236f8c935d6ffbc0feb931839
eduardotedeschi <eduardoroberto9a@gmail.com>
2025-01-16 19:33:09 -0300

    Conclusão Seção 3

--- aula48/assets/js/main.js
+++ aula48/assets/js/main.js
@@ -2,20 +2,36 @@ function escopo(){
     const inputTarefa = document.querySelector('#input-tarefa');
     const listaTarefas = document.querySelector('.lista-tarefas');
     
+    
+    function salvarTarefas(){
+        const liTarefas = listaTarefas.querySelectorAll('li');
+        const textoTarefas = [];
+        
+        for(let tarefa of liTarefas){
+            let text = tarefa.innerText;
+            text = text.replace('Apagar', '').trim();
+            textoTarefas.push(text);
+        }
+        const tarefasJSON = JSON.stringify(textoTarefas);
+        localStorage.setItem('tarefas', tarefasJSON);
+    }
+    
     function criaBotaoApagar(li){
         const botao = document.createElement('button');
-        botao.classList.add('button-apagar-tarefa');
+        li.innerText += ' ';
+        botao.setAttribute('class', 'button-apagar-tarefa'); ///////////ANOTAR QUE ESSA FUNÇÃO SETA UMA ATRIBUTO, PODE SER REF NO <A>, POR EXEMPLO
+        botao.setAttribute('title', 'Irá apagar essa tarefa');
         botao.innerText = 'Apagar';
         li.appendChild(botao);
     }
     
     function limpaInput(input){
         input.value = '';
-        input.focus(); ///////////ANOTAR QUE ESSE COMANDO DEIXA SELECIONADO (FOCADO) O ITEM
+        input.focus(); ///////////ANOTAR QUE ESSA FUNÇÃO DEIXA SELECIONADO (FOCADO) O ITEM
     }
     
     const criaLi = () => document.createElement('li');
-
+    
     function criaTarefa(textoInput){
         const tarefa = criaLi();
         const texto = document.createTextNode(textoInput);
@@ -23,8 +39,9 @@ function escopo(){
         listaTarefas.appendChild(tarefa);
         limpaInput(inputTarefa);
         criaBotaoApagar(tarefa);
+        salvarTarefas();
     }
-
+    
     inputTarefa.addEventListener('keypress', function(e){  ///////////ANOTAR QUE TEM COMO PEGAR A TECLA PRESSIONADA USANDO EVENTO
         if(e.key === 'Enter'){
             if(!inputTarefa.value) return;
@@ -39,8 +56,19 @@ function escopo(){
             criaTarefa(inputTarefa.value);
         }
         if(elemento.classList.contains('button-apagar-tarefa')){
-
+            elemento.parentElement.remove();
+            salvarTarefas();
         }
     });
+    
+    function recuperaTarefas(){
+        const tarefas = localStorage.getItem('tarefas');
+        const textoTarefas = JSON.parse(tarefas);
+    
+        for(let tarefa of textoTarefas){
+            criaTarefa(tarefa);
+        }
+    }
+    recuperaTarefas();
 }
 escopo();
