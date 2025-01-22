d79b8e5f2ca0142c40009f262ca91804b75c0424
eduardotedeschi <eduardoroberto9a@gmail.com>
2025-01-22 11:28:13 -0300

    Seção 4 aula 77

---
+++ aula57/assets/js/main.js
@@ -0,0 +1,81 @@
+(function(){
+    function criaCalculadora(){
+        return {
+            display: document.querySelector('.display'),
+            btnDel: document.querySelector('.btn-del'),
+            btnEq: document.querySelector('.btn-eq'),
+
+            inicia(){
+                this.cliqueBotoes();
+                this.pressionaEnter();
+            },
+
+            clearDisplay(){
+                this.display.value = '';
+            },
+
+            deletarChar(){
+                this.display.value = this.display.value.slice(0, -1);
+                this.display.focus();
+            },
+
+            calculo(){
+                let conta = this.display.value;
+
+                try{
+                    conta = eval(conta);
+                    if(!conta){
+                        alert('Conta inválida!');
+                        return;
+                    }
+                    this.display.value = conta;
+
+                }catch(e){
+                    alert('Conta inválida!');
+                    return;
+                }
+            },
+
+            cliqueBotoes(){
+                //this --> calculadora
+                document.addEventListener('click', (e) =>{
+                    //this --> document apenas quando é function, quando é arrow function, o this continua o mesmo
+                    const el = e.target;
+                    if(el.classList.contains('btn-num')){
+                        this.bntParaDisplay(el.innerText);
+                    }
+
+                    if(el.classList.contains('btn-clear')){
+                        this.clearDisplay();
+                    }
+
+                    if(el.classList.contains('btn-del')){
+                        this.deletarChar();
+                    }
+
+                    if(el.classList.contains('btn-eq')){
+                        this.calculo();
+                    }
+                });
+            },
+
+            bntParaDisplay(botao){
+                this.display.value += botao;
+                this.display.focus();
+            },
+
+            pressionaEnter(){
+                this.display.addEventListener('keyup', (e) => {
+                    if (e.key === 'Enter'){
+                        this.calculo();
+                    }
+                });
+            }
+
+        };
+    };
+
+    const calculadora = criaCalculadora();
+    calculadora.inicia();
+
+})();
---
+++ aula58/index.js
@@ -0,0 +1,23 @@
+//Função contrutora ---> retorna objetos
+//Contrutora --> Pessoa (new)
+
+function Pessoa(nome, sobrenome){
+    //Atributos ou métodos privados
+    const ID = 123456;
+    const metodoInte = function (){
+
+    };
+
+    //Atributos ou métodos públicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodoPublico = function (){
+        console.log(this.nome + ': sou um método.');
+    };
+}
+
+const p1 = new Pessoa('Eduardo', 'Tedeschi');
+const p2 = new Pessoa('Gislene', 'Auxiliadora');
+
+p1.metodoPublico();
