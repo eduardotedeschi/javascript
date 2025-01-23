6b854e10c12c6c7858a089a41a4434d22e8fe993
Eduardo Roberto de Castro Tedeschi <eduardoroberto9a@gmail.com>
2025-01-23 14:43:45 -0300

    Seção 4 aula 78 2/2

--- aula59/assets/js/main.js
+++ aula59/assets/js/main.js
@@ -7,6 +7,8 @@
 
         this.iniciar = function() {
             clicaBotao();
+            pressionaEnter();
+            display.focus();
         };
 
         const mostrarDisplay = function(valor) {
@@ -21,23 +23,56 @@
             display.value = display.value.slice(0, -1);
         };
 
+        const calcular = function() {
+            try {
+                let conta = display.value;
+                conta = eval(conta);
+                if(!conta){
+                    alert('Erro na conta!');
+                    display.value = '';
+                }
+                display.value = conta;
+            } catch (error) {
+                const err = error;
+                alert('Erro na conta!');
+                display.value = '';
+            }
+        };
+
         const clicaBotao = function() {
             calculadora.addEventListener('click', e => {
                 const el = e.target;
 
                 if(el.classList.contains('btn-num')){
                     mostrarDisplay(el.innerText);
+                    display.focus();
                 }
 
                 if(el.classList.contains('btn-clear')){
                     limpaDisplay();
+                    display.focus();
                 }
 
                 if(el.classList.contains('btn-del')){
                     deletarChar();
+                    display.focus();
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    calcular();
+                    display.focus();
                 }
             });
         };
+
+        const pressionaEnter = function() {
+            document.addEventListener('keyup', e => {
+                if(e.key === 'Enter'){
+                    calcular();
+                    display.focus();
+                }
+            });
+        }
     }
 
     const calculadora1 = new Calculadora();
